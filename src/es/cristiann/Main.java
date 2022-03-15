80db8a9fdfd4a9c92cd5d736b1a1babbe2f51e4a
Cristian García Hernández <al036933@ieslaencanta.com>
2022-03-15 12:34:57 +0100

    Commit inicial.

---
+++ src/es/cristiann/Main.java
@@ -0,0 +1,14 @@
+package es.cristiann;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String texto;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Escribe todo lo que quieras (salir para terminar)... Empieza cuando quieras ");
+        texto = entrada.nextLine();
+    }
+}
